0fb6a2e4a8030d0b635c2bf9f836d8d71213f569
Josh Kraines <krainesjosh@gmail.com>
2023-09-05 20:48:57 -0400

    non firebase related changes

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,14 @@
+import {styled} from "styled-components";
+
+const NewsHolder = styled.iframe`
+  height: 100vh;
+  width: 100vw;
+`;
+
 export default function News() {
     return (
-        <>News</>
+        <>
+            <NewsHolder src="https://trevormart.in/FantasyFootball22/Week15/Week15.html" />
+        </>
     );
 };
