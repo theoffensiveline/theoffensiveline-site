392512d4f85fae8329ff3a4ac096ac0e63baf39a
Josh Kraines <krainesjosh@gmail.com>
2023-09-24 12:59:50 -0400

    fixed mobile nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,27 +5,35 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['News', 'Submit'];
 
-export default function NavBar() {
+function NavBar() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
 
-    const handleCloseNavMenu = (page) => {
-        setAnchorElNav(page);
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
     };
+
+    const redirect = (page) => {
+        window.location.href = page.toLowerCase();
+    }
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
+                    {/*<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />*/}
                     <Typography
                         variant="h6"
                         noWrap
@@ -51,9 +59,12 @@ export default function NavBar() {
                             aria-haspopup="true"
                             onClick={handleOpenNavMenu}
                             color="inherit"
-                        />
+                        >
+                            <MenuIcon />
+                        </IconButton>
                         <Menu
                             id="menu-appbar"
+                            anchorEl={anchorElNav}
                             anchorOrigin={{
                                 vertical: 'bottom',
                                 horizontal: 'left',
@@ -70,12 +81,13 @@ export default function NavBar() {
                             }}
                         >
                             {pages.map((page) => (
-                                <MenuItem key={page}>
+                                <MenuItem key={page} onClick={() => redirect(page)}>
                                     <Typography textAlign="center">{page}</Typography>
                                 </MenuItem>
                             ))}
                         </Menu>
                     </Box>
+                    {/*<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />*/}
                     <Typography
                         variant="h5"
                         noWrap
@@ -87,18 +99,17 @@ export default function NavBar() {
                             flexGrow: 1,
                             fontFamily: 'monospace',
                             fontWeight: 700,
-                            letterSpacing: '.3rem',
                             color: 'inherit',
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        The Offensive Line
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={() => window.location.href = page.toLowerCase()}
+                                onClick={() => redirect(page)}
                                 sx={{ my: 2, color: 'white', display: 'block' }}
                             >
                                 {page}
@@ -110,3 +121,4 @@ export default function NavBar() {
         </AppBar>
     );
 }
+export default NavBar;
