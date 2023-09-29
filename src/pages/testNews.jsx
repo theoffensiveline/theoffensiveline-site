4ebaef56b90771b79de3259aec36f4d3f2e020ca
Trevor Martin <117135663+trevormmartin@users.noreply.github.com>
2023-09-29 10:06:12 -0400

    add react responsive masonry

--- src/pages/testNews.jsx
+++ src/pages/testNews.jsx
@@ -1,51 +1,23 @@
-import { styled } from "styled-components";
+import React from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function TestNews() {
+    const items = Array.from({ length: 100 }).map((_, index) => (
+        <img
+            key={index}
+            src={`https://picsum.photos/200/${Math.floor(
+                Math.random() * (300 - 200 + 1) + 200
+            )}`}
+            style={{ width: "100%", borderRadius: "8px" }}
+        />
+    ));
     return (
-        <>
-            <NewspaperHolder>
-                <ArticleHolder>
-                    <div>What is Lorem Ipsum?
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-                </ArticleHolder>
-                <ArticleHolder>
-                    <div>Why do we use it?
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-                        Where does it come from?
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-                        Where can I get some?
-                        There are many variations of passages of Lorem Ipsum available, </div>
-                </ArticleHolder>
-                <ArticleHolder>
-                    <div>Testing</div>
-                </ArticleHolder>
-                <ArticleHolder>
-                    <div>Testing</div>
-                </ArticleHolder>
-            </NewspaperHolder>
-        </>
+        <div className="TestNews">
+            <ResponsiveMasonry
+                columnsCountBreakPoints={{ 300: 2, 500: 3, 700: 4, 900: 5 }}
+            >
+                <Masonry>{items}</Masonry>
+            </ResponsiveMasonry>
+        </div>
     );
-};
-
-const NewspaperHolder = styled.div`
-    display: grid;
-    width: 100%;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    grid-template-rows: 1fr auto;
-    min-height: 100%;
-    background-color: grey;
-`;
-
-const ArticleHolder = styled.div`
-    display: grid;
-    border: 1px solid black;
-    background-color: #f9f7f1;
-    padding: 16px;
-    margin: 8px;
-    height: fit-content;
-`;
+}
