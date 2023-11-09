bfdfba660e245266ed3bebe8b4262c3215c8e4fe
trevormmartin <trvmart2@gmail.com>
2023-11-09 17:56:26 -0500

    Update News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -8,7 +8,7 @@ const NewsHolder = styled.iframe`
 export default function News() {
     return (
         <>
-            <NewsHolder src="https://trevormart.in/FantasyFootball23/Week8/Week8.html" />
+            <NewsHolder src="https://trevormart.in/FantasyFootball23/Week9/Week9.html" />
         </>
     );
 };
